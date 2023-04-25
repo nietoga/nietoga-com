dc98bcc0c016a5d861c3ca99a7977d14d8e83b06
Agustin Nieto <agusnieto.gg@gmail.com>
2023-04-25 07:36:11 -0500

    Wrap to remove warning

--- src/component/py-script/runtime.tsx
+++ src/component/py-script/runtime.tsx
@@ -8,9 +8,9 @@ interface Props {
 export const PyScriptRuntime = ({children}: Props) => {
     return <>
         <Head>
-            <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css"/>
+            <link rel="stylesheet" href={"https://pyscript.net/latest/pyscript.css"}/>
         </Head>
-        <Script src="https://pyscript.net/latest/pyscript.js"/>
+        <Script src={"https://pyscript.net/latest/pyscript.js"}/>
         {children}
     </>
 }
