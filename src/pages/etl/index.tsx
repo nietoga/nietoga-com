8d5a65e2bab971151040fccba7e7a0cd19c3fedb
Agustin Nieto <agusnieto.gg@gmail.com>
2023-07-19 00:16:09 -0500

    Add functional ETL - shitty UI and code

---
+++ src/pages/etl/index.tsx
@@ -0,0 +1,22 @@
+import { Form } from '@nietoga/nietoga-com/component/etl/form';
+import { Meta, Title } from '@nietoga/nietoga-com/component/page';
+import { PythonProvider } from 'react-py';
+
+export const Index = () => {
+    return (
+        <>
+            <Title title="ETL" />
+            <Meta
+                name="description"
+                content="Page for executing ETL operations using py-script"
+            />
+            <main>
+                <PythonProvider packages={{ official: ['pandas'] }}>
+                    <Form />
+                </PythonProvider>
+            </main>
+        </>
+    );
+};
+
+export default Index;
