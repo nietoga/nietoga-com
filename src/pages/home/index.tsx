b2ba430f8cdc2477efd0048a6477a988e384b9f7
evinracher <eevinley@gmail.com>
2023-07-25 11:13:38 -0500

    feat(dashboard): improve home page with a projects dashboard

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,11 @@
-import { Meta, Title } from '@nietoga/nietoga-com/components/page';
-import Image from 'next/image';
-import Link from 'next/link';
 import styles from './index.module.css';
-import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
+
+// Components
+import Image from 'next/image';
+import { Box } from '@mui/material';
 import { Layout } from '../../components/layout';
+import { Meta, Title } from '../../components/page';
+import { ProjectsList } from '../../components/projectsList/projectsList';
 
 export const Home = () => {
     return (
@@ -13,20 +14,12 @@ export const Home = () => {
             <Meta name="description" content="Home page of nietoga-com" />
             <Box className={styles.main}>
                 <Image
-                    src="/nyan-cat.gif"
+                    src="/images/nyan-cat.gif"
                     alt="Silly image of a space cat throwing rainbows down its ass"
                     width={500}
                     height={500}
                 />
-
-                <Box>
-                    <Typography variant="h3">Projects</Typography>
-                    <ul>
-                        <li>
-                            <Link href="/etl">ETL module</Link>
-                        </li>
-                    </ul>
-                </Box>
+                <ProjectsList />
             </Box>
         </Layout>
     );
